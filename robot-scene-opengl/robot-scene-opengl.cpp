7c3ce37cb8b37488dec8ea22335d5bcaebe4bc89
glusi <glusi3619@gmail.com>
2022-06-28 17:09:33 +0300

    Main template

--- robot-scene-opengl/robot-scene-opengl.cpp
+++ robot-scene-opengl/robot-scene-opengl.cpp
@@ -1,20 +1,70 @@
-// robot-scene-opengl.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <GL/glut.h>
+#include <math.h>
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <string>
+#include <GL\freeglut_ext.h>
+#include <algorithm>
 #include <iostream>
+#include <vector>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+//Default values for initial window
+constexpr auto WINDOW_WIDTH = 800;
+constexpr auto WINDOW_HEIGHT = 500;
+constexpr auto WINDOW_POSITION_X = 50;
+constexpr auto WINDOW_POSITION_Y = 50;
+
+
+//the current width and height of the window
+GLsizei new_width, new_height;
+
+
+void display() {
+    //Clears
+    glClearColor(1.0, 1.0, 1.0, 0.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+
+
+    glFlush();
+    glutSwapBuffers();
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+// Handle pressing any key, so that user can switch between Orthogonal and Perspective projection
+void MyKeyboardFunc(unsigned char Key, int x, int y)
+{
+
+}
+
+// handles window reshape	
+void windowResize(int width, int height) {
+    //calculate scene  new scale width and hight
+    float scale_width = width / (float)WINDOW_WIDTH;
+    float scale_height = height / (float)WINDOW_HEIGHT;
+    //find the deciding scale
+    float scale = (scale_width > scale_height) ? scale_height : scale_width;
+    //calculate new window's scale width and hight
+    new_width = scale * WINDOW_WIDTH;
+    new_height = scale * WINDOW_HEIGHT;
+    //Set new viewport
+    glViewport(0, height - new_height, new_width, new_height);
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
+    //Set initial parameters for the window
+    glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    glutInitWindowPosition(WINDOW_POSITION_X, WINDOW_POSITION_Y);
+    glutCreateWindow("robot-scene");
+    glutDisplayFunc(display);
+    glutReshapeFunc(windowResize);
+    glutKeyboardFunc(MyKeyboardFunc);
+    glutMainLoop();
+    return 0;
+}
+
