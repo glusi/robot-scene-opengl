44a80865981e442d5750dcd2ebf2c1de5705751a
glusi <glusi3619@gmail.com>
2022-08-11 04:03:09 +0300

    SceneItem Docu

--- robot-scene-opengl/SceneItem.cpp
+++
@@ -1,3 +0,0 @@
-#include "SceneItem.h"
-
-
--- robot-scene-opengl/SceneItem.h
+++ robot-scene-opengl/SceneItem.h
@@ -3,11 +3,18 @@
 #include "Vector3.h"
 #include "Materials.h"
 
+/**
+ * A base class for all drawable objects in scene.
+ */
 class SceneItem
 {
 protected:
 	
 public:
-	void draw();
+	/**
+	 * A virtual draw function to be overridden.
+	 * 
+	 */
+	virtual void draw() = 0;
 };
 
